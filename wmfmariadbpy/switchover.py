7a534b762d91560d679f3d3441a27f1ed6db6924
Jaime Crespo <jcrespo@wikimedia.org>
2020-07-07 07:57:07 +0200

    switchover.py: Split zarcillo and tendril reference
    Zarcillo and tendril are no longer on the same instance,
    set them on on different constants.
    
    This is a followup to: 8ee0e032d4043e4
    
    Change-Id: Ib32bb163265ee5c7baeaf249c6b1ac4b0940d463

--- wmfmariadbpy/switchover.py
+++ wmfmariadbpy/switchover.py
@@ -16,6 +16,7 @@ HEARTBEAT_EXEC = '/usr/bin/perl /usr/local/bin/pt-heartbeat-wikimedia --defaults
 --interval={2} --set-vars=binlog_format=STATEMENT -S {3} --daemonize \
 --pid /var/run/pt-heartbeat.pid'
 
+TENDRIL_INSTANCE = 'db1115'  # instance_name:port format
 ZARCILLO_INSTANCE = 'db2093'  # instance_name:port format
 
 
@@ -456,7 +457,7 @@ def update_tendril(master, slave):
     """
     print('Updating tendril...')
     # get section of the original master
-    tendril = WMFMariaDB(ZARCILLO_INSTANCE, database='tendril')
+    tendril = WMFMariaDB(TENDRIL_INSTANCE, database='tendril')
     query = ("SELECT name "
              "FROM shards "
              "WHERE master_id = (SELECT id "
