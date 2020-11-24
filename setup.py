b6cc32c12f84db8f04e4afca9daad0a3cba38d29
Jaime Crespo <jcrespo@wikimedia.org>
2020-11-24 12:47:00 +0100

    Update package to version 0.4
    See changelog for details. Depends on future wmfbackups 0.6.
    
    Change-Id: I2d262b59d8910b47ab64a92787a1cfddcffa69c6

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ with open('README.md', 'r') as fh:
 setup(
     name='wmfbackups',
     description='wmfbackups',
-    version='0.3',
+    version='0.4',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://phabricator.wikimedia.org/diffusion/OSWB/",
     packages=['wmfbackups'],
     install_requires=['pymysql>=0.9.3',
-                      # Pending release of wmfmariadbpy v0.6:
-                      'wmfmariadbpy @ git+https://gerrit.wikimedia.org/r/operations/software/wmfmariadbpy@3f7a934'],
+                      'wmfmariadbpy @ git+https://gerrit.wikimedia.org/r/operations/software/wmfmariadbpy@v0.6'],
     entry_points={
         'console_scripts': [
            # cli
