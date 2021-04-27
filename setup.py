725941048599f92a6d673e416c5bce5eb27c940b
Jaime Crespo <jcrespo@wikimedia.org>
2021-04-27 11:58:56 +0200

    Release new v0.5 version
    Release a new package version for wmfbackups, with all bugfixes
    applied since v0.4.
    
    Bug: T281094
    Change-Id: I7bf6e43e10b3c9732d2d92de2722b16951bc8ccb

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 setup(
     name='wmfbackups',
     description='wmfbackups',
-    version='0.4',
+    version='0.5',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://phabricator.wikimedia.org/diffusion/OSWB/",
